47f0609d007e9cbc9f74632b2b377bc687a5c449
Petros Kourampas <pkourampas@gmail.com>
2025-01-26 21:42:45 +0200

    remove the count from nat_gateway in route section on private subnet file

--- private_subnet.tf
+++ private_subnet.tf
@@ -18,7 +18,7 @@ resource "aws_route_table" "private" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_gw[count.index].id
+    nat_gateway_id = aws_nat_gateway.nat_gw[0].id
   }
 }
 
