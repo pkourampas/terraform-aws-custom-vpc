353b169b8879d0ce10fd0f108742a7fd8a4ba770
Petros Kourampas <pkourampas@gmail.com>
2025-02-22 21:35:19 +0200

    add a region for the s3 bucket

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ provider "aws" {
 module "s3_backend" {
   source = "./modules/s3_bucket"
   name = var.s3-bucket-name
-  s3-bucket-region = lookup(var.aws-region, "Canada")
+  s3-bucket-region = "ca-central-1"
 }
 
 # Create VPC with IPv4 CIDR block of 254 IPs (var.aws_vpc_cidr)
