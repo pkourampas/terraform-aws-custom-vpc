0dbdcf0bb0c5e1cffb183b9630f3e74fb182ad29
Petros Kourampas <pkourampas@gmail.com>
2025-02-22 21:26:27 +0200

    could not setup local value in s3 region

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ provider "aws" {
 module "s3_backend" {
   source = "./modules/s3_bucket"
   name = var.s3-bucket-name
-  s3-bucket-region = local.selected_region
+  s3-bucket-region = lookup(var.aws-region, "Canada")
 }
 
 # Create VPC with IPv4 CIDR block of 254 IPs (var.aws_vpc_cidr)
